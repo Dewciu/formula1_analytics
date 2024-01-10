def43e35e8276cdd33b9cafef07ec259b87c1855
Dewciu <kacperkrol@protonmail.com>
2024-01-10 16:45:22 +0100

    initial

---
+++ src/config/config.py
@@ -0,0 +1,3 @@
+RAW_DATA_PATH = (
+    "/Users/dewciu/Development/studies/object_languages/project/formula1_analytics/data"
+)
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from common.data_loader import DataLoader
+
+if __name__ == "__main__":
+    print(DataLoader("drivers.csv").load_data())
