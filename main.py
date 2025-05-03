c72370ce3af61f5a97e86f1de73b2627d144747b
Dewciu <97891254+Dewciu@users.noreply.github.com>
2025-05-03 14:37:33 +0200

    feat: plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from formula1_analytics.drivers.driver_weather_perf import DriverWeatherPerf, WeatherType
+from formula1_analytics.drivers.drivers_season_perf import DriversSeasonPerf
 
 
 if __name__ == "__main__":
@@ -8,8 +9,14 @@ if __name__ == "__main__":
     
     rainfall_perf = driver_weather.get_data(
         season_year=season_year,
-        driver_names=driver_names,
-        weather_type=WeatherType.WIND_SPEED
+        driver_names=None,
+        weather_type=WeatherType.PRESSURE
     )
 
     print(rainfall_perf)
+
+    season_perf = DriversSeasonPerf().get_data(
+        season_year=season_year,
+        driver_names=None
+    )
+    print(season_perf.columns.to_list())
--- src/formula1_analytics/drivers/drivers_plots.py
+++ src/formula1_analytics/drivers/drivers_plots.py
@@ -5,6 +5,7 @@ import matplotlib.axes as plta
 import matplotlib.pyplot as plt
 from formula1_analytics.drivers.drivers_season_perf import DriversSeasonPerf
 from formula1_analytics.drivers.drivers_most_wins import DriversMostWins
+from formula1_analytics.drivers.driver_weather_perf import DriverWeatherPerf
 
 
 class DriversPlots:
@@ -66,6 +67,35 @@ class DriversPlots:
 
         img_bytes = BytesIO()
         plt.savefig(img_bytes, format="png", bbox_inches="tight")
+        plt.close()
+
+        return img_bytes.getvalue()
+    
+
+    @staticmethod
+    def get_plot_drivers_weather_perf(
+        season_year: int,
+        driver_name: str = None,
+        weather_type: str = None,
+    ) -> plta.Axes:
+        data = DriverWeatherPerf().get_data(season_year, [driver_name], weather_type)
+        driver_weather_perf = data[["fullname", "weather_category", "avg_position"]]
+        colors = plt.cm.Paired(np.linspace(0, 0.8, len(driver_weather_perf.columns)))
+
+        drivers_weather_perf_plot = driver_weather_perf.plot(
+            figsize=(25, 20),
+            grid=True,
+            lw=7,
+            color=colors,
+        )
+        drivers_weather_perf_plot.set_title(
+            f"{driver_name} performance in {weather_type} in {season_year} season", fontdict={"fontsize": 40}
+        )
+        drivers_weather_perf_plot.set_xlabel("Round", fontdict={"fontsize": 20})
+        drivers_weather_perf_plot.set_ylabel("Points", fontdict={"fontsize": 20})
+        drivers_weather_perf_plot.legend(fontsize=20)
+        img_bytes = BytesIO()
+        plt.savefig(img_bytes, format="png")
         plt.close()
 
         return img_bytes.getvalue()
